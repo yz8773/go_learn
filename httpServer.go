933ed1e3e7ba67b114d1090aae10c4644a9661a2
yangzhao9 <yangzhao9@jd.com>
2022-06-05 23:22:08 +0800

    --http server

---
+++ httpServer.go
@@ -0,0 +1,76 @@
+package goHttpServer
+
+import (
+	"fmt"
+	"log"
+	"net"
+	"net/http"
+	"net/http/pprof"
+	"os"
+	"strings"
+)
+
+func index(w http.ResponseWriter, r *http.Request) {
+	os.Setenv("VERSION", "v0.0.1")
+	version := os.Getenv("VERSION")
+	w.Header().Set("VERSION", version)
+	fmt.Printf("os version : %s \n", version)
+
+	for k, v := range r.Header {
+		for _, vv := range v {
+			fmt.Printf("Header key : %s, Header value : %s \n", k, v)
+			w.Header().Set(k, vv)
+		}
+	}
+
+	clientIp := getCurrentIp(r)
+
+	log.Printf("Success! Response code : %d", 200)
+	log.Printf("Success! clientIp : %s", clientIp)
+
+}
+
+func getCurrentIp(r *http.Request) string {
+	ip := r.Header.Get("X-Real-IP")
+	if ip == "" {
+		ip = strings.Split(r.RemoteAddr, ":")[0]
+
+	}
+
+	return ip
+}
+
+func ClientIp(r *http.Request) string {
+	xForwardedFor := r.Header.Get("X-Forwarded-For")
+	ip := strings.TrimSpace(strings.Split(xForwardedFor, ",")[0])
+	if ip != "" {
+		return ip
+	}
+
+	ip = strings.TrimSpace(r.Header.Get("X-Real-IP"))
+	if ip != "" {
+		return ip
+	}
+
+	if ip, _, err := net.SplitHostPort(strings.TrimSpace(r.RemoteAddr)); err == nil {
+		return ip
+	}
+	return ""
+}
+
+func healthz(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "working")
+}
+
+func main() {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/debug/pprof/", pprof.Index)
+	mux.HandleFunc("/debug/pprof/profile", pprof.Profile)
+	mux.HandleFunc("/debug/pprof/symbol", pprof.Symbol)
+	mux.HandleFunc("/debug/pprof/trace", pprof.Trace)
+	mux.HandleFunc("/", index)
+	mux.HandleFunc("/healthz", healthz)
+	if err := http.ListenAndServe(":8080", mux); err != nil {
+		log.Fatalf("start http server failed, error: %s \n", err.Error())
+	}
+}
